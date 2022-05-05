d9b1334359c4eb4e7e3fe7f583616c0b2cbcbd18
sam-brk <sberkay64@gmail.com>
2022-05-05 12:39:33 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sam's Website</h1>
+      <h2>Welcome to my website :)</h2>
+    </div>
+  );
+}
+
+export default App;
